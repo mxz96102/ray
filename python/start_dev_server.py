813c14e463242276c748f668a239800c8ce56ae4
刘宝 <po.lb@antfin.com>
2020-04-17 11:59:09 +0800

    Add start_server.py for dev

---
+++ python/start_dev_server.py
@@ -0,0 +1,36 @@
+import os
+import ray
+import pprint
+import time
+
+os.environ.pop('http_proxy')
+os.environ.pop('https_proxy')
+
+r = ray.init(num_cpus=2, num_gpus=2)
+pprint.pprint(r)
+
+
+@ray.remote(num_cpus=1)
+class B(object):
+    pass
+
+
+@ray.remote
+class A(object):
+    def foo(self):
+        B.remote()
+        time.sleep(100000)
+
+
+@ray.remote
+def bar():
+    time.sleep(100000)
+
+
+a = A.remote()
+a.foo.remote()
+
+b = bar.remote()
+
+while True:
+    time.sleep(100000)
